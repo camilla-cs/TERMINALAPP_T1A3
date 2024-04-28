706c20c5ba7478f0c4485ee145197920ae76687d
camilla-cs <15404@coderacademy.edu.au>
2024-04-28 15:07:55 +1000

    add note function and notebook menu added

--- notebook_functions.py
+++ notebook_functions.py
@@ -1,13 +1,22 @@
 import csv
+def notebook_menu():
+    print("You open the notebook. What would you like to do? ")
+    print("1. Add note. ")
+    print("2. Remove note. ")
+    print("3. Edit note. ")
+    print("4. View notes. ")
+    print("5. Quit the game. ")
+    user_choice = input("\nPlease enter a number from above. ")
+    return user_choice
 
 def add_note (file_name):
-    print("Add note: ")
-    with open(file_name,"a") as n: 
+    note = input ("Enter your note: ")
+    with open(file_name,"a", newline="") as n: 
         writer = csv.writer(n)
-        writer.writerow ()
+        writer.writerow ([note])
 
 def remove_note (file_name): 
-    print("Remove note: ")
+    pass
 
 
 
